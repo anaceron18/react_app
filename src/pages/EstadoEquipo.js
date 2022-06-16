6fe7ecd69b27e5fb8e32b294f52a060082c4d7b7
anaceron18 <anaceron18@gmail.com>
2022-06-16 15:28:07 -0500

    Version con conexion front

--- src/pages/EstadoEquipo.js
+++ src/pages/EstadoEquipo.js
@@ -1,5 +1,28 @@
-const Blogs = () => {
-    return <h1>Estado Equipo 5</h1>;
-  };
-  
-  export default Blogs;
+
+import React from 'react';
+import axios from 'axios';
+
+
+export default class Blogs extends React.Component {
+  state = {
+    Usuario: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:4000/usuario`)
+      .then(res => {
+        console.log(res);
+        const Usuario = res.data;
+        this.setState({ Usuario });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.Usuario.map(Usuario => <li>{Usuario.nombre}</li>)}
+      </ul>
+    )
+  }
+}
+
--- src/pages/TipoEquipo.js
+++ src/pages/TipoEquipo.js
@@ -1,5 +1,42 @@
-const Blogs = () => {
-  return <h1>Tipo Equipo</h1>;
-};
 
-export default Blogs;
+import React from 'react';
+import axios from 'axios';
+
+export default class Blogs extends React.Component {
+ 
+  handleSubmit = event => {
+    event.preventDefault();
+   
+    axios.post(`http://localhost:4000/usuario?`, {
+      email: document.getElementById("email").value,
+      nombre:document.getElementById("nombre").value,
+      estado:document.getElementById("estado").value
+    })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Nombre:
+            <input type="text" id="nombre"  />
+          </label>
+          <label>
+           Email:
+            <input type="text" id="email" />
+          </label>
+          <label>
+           Estado:
+            <input type="text" id="estado"/>
+          </label>
+          <button type="submit">Agregar Usuario</button>
+        </form>
+      </div>
+    )
+  }
+}
+
